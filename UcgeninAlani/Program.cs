75ab3a2caa2b30a829570d79aad86afd2cf62751
berkaykarakush <berkaykaraks@outlook.com>
2022-10-27 13:54:10 +0300

    Project created

---
+++ UcgeninAlani/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 10;
+int b = 20;
+int toplam;
+toplam = a + b;
+Console.WriteLine(toplam);
