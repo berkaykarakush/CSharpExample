b393e075f350604828a8afa8696d704bf303a471
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:20:49 +0300

    TamsayiyiTerstenYazdirma added

---
+++ TamsayiyiTerstenYazdirma/Program.cs
@@ -0,0 +1,11 @@
+﻿int sayi, kalan;
+
+Console.Write("Tamsayi giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+kalan = sayi;
+Console.Write("Tamsayinin tersten yazilmis hali: ");
+while(kalan!=0){
+     Console.Write(kalan%10);
+     kalan /=10;
+}
