96dfb1a9f78d312de9d33b98972ea12cb7969d89
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 14:27:38 +0300

    AkrepYelkovanAcisiHesaplama added

---
+++ AkrepYelkovanAcisiHesaplama/Program.cs
@@ -0,0 +1,12 @@
+﻿int saat, dakika, ilerleme;
+
+Console.Write("Saat (0-11): ");
+saat = Convert.ToInt32(Console.ReadLine());
+Console.Write("Dakika (0-59): ");
+dakika = Convert.ToInt32(Console.ReadLine());
+
+ilerleme = dakika/2;
+saat *=30;
+dakika *= 6;
+
+Console.WriteLine("{0},{1}",saat+ilerleme,dakika);
