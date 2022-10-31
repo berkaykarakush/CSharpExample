b9a9613b69f17377439700af0c8e491b22f9ba72
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 14:23:37 +0300

    ArtikYilHesaplama Added

---
+++ ArtikYilHesaplama/Program.cs
@@ -0,0 +1,11 @@
+﻿int yil;
+
+Console.Write("Yil degerini giriniz: ");
+yil = Convert.ToInt32(Console.ReadLine());
+
+if((yil % 4 ==0) && ((yil % 100 != 0) || (yil % 400 == 0))){
+    Console.Write("{0} degeri bir artik yildir.",yil);
+}
+else{
+    Console.Write("{0} degeri bir artik yil degildir.",yil);
+}
