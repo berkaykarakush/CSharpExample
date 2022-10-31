db51331779ccc5a942d43f22262570e82fb36d58
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:26:54 +0300

    SayininTerstenSayisalDegeriniHesaplama added

---
+++ SayininTerstenSayisalDegeriniHesaplama/Program.cs
@@ -0,0 +1,12 @@
+﻿int sayi, terstenToplam=0, kalan;
+
+Console.Write("Tamsayi degerini giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+while(sayi!=0){
+    kalan = sayi % 10;
+    terstenToplam = terstenToplam*10+kalan;
+    sayi = sayi/10;
+}
+
+Console.Write("Sayinin tersten toplami: {0}",terstenToplam);
