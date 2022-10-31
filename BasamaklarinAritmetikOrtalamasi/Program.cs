c7c1c6404dcb18c6b5a9319a5cb14925015205cf
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 16:00:54 +0300

    BasamaklarinAritmetikOrtalamasi added

---
+++ BasamaklarinAritmetikOrtalamasi/Program.cs
@@ -0,0 +1,17 @@
+﻿int sayi, birBasamagi, onBasamagi, yuzBasamagi, ort=0;
+
+do
+{
+    Console.Write("3 basamakli pozitif bir tamsayi giriniz: ");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    birBasamagi = sayi%10;
+    onBasamagi = (sayi/10)%10;
+    yuzBasamagi = sayi/100;
+    ort = (birBasamagi+onBasamagi+yuzBasamagi)/3;
+
+} while ((sayi<=100)||(sayi>999));
+
+Console.Write("Ortalama: {0}",(double)ort);
+
+
+
