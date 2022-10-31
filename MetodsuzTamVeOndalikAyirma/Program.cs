51852d680a81a62cbdff9b70fd1fff3daaae38f9
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:11:46 +0300

    MetodsuzTamVeOndalikAyirma added

---
+++ MetodsuzTamVeOndalikAyirma/Program.cs
@@ -0,0 +1,10 @@
+﻿double sayi, ondalikliKisim;
+int tamKisim;
+
+Console.Write("Bir sayi degeri giriniz: ");
+sayi = Convert.ToDouble(Console.ReadLine());
+
+tamKisim = (int)sayi;
+ondalikliKisim = sayi-tamKisim;
+
+Console.Write("Girilen sayi degerinin tam kismi: {0}\nGirilen sayi degerinin ondalikli kismi: {1:F2}",tamKisim,ondalikliKisim);
