c4e0753f249274add7f99f01bf7e9d9c0fe713a8
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 14:56:30 +0300

    DegiskenIcerikleriniDegistirme added

---
+++ DegiskenIcerikleriniDegistirme/Program.cs
@@ -0,0 +1,12 @@
+﻿int sayi1, sayi2, tutucu;
+
+Console.Write("Ilk sayi degerini giriniz: ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ikinci sayi degerini giriniz: ");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+tutucu = sayi1;
+sayi1 = sayi2;
+sayi2 = tutucu;
+
+Console.Write("\nSayi 1: {0}\nSayi 2: {1}",sayi1,sayi2);
