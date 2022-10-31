992c9476e5909f91226f946cf50c3017033b355d
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:49:13 +0300

    PayVePaydaDegerineGoreBasamakHesaplama added

---
+++ PayVePaydDegerineGoreBasamakHesaplama/Program.cs
@@ -0,0 +1,16 @@
+﻿int pay, payda, n, basamakDegeri=0;
+
+Console.Write("Pay: ");
+pay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Payda: ");
+payda = Convert.ToInt32(Console.ReadLine());
+Console.Write("n: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+for(int i=0; i<n; i++){
+    basamakDegeri = pay/payda;
+    pay %= payda;
+}
+
+Console.Write("Basamak degeri: {0}",basamakDegeri);
