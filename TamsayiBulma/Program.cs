ebfa118cba7e42334aced003e7e521c45d16698f
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:01:24 +0300

    TamsayiBulma added

---
+++ TamsayiBulma/Program.cs
@@ -0,0 +1,11 @@
+﻿double sayi;
+
+Console.Write("Bir sayi degerini giriniz: ");
+sayi =Convert.ToDouble(Console.ReadLine());
+
+if(sayi == Math.Round(sayi)){
+    Console.Write("Tam sayidir.");
+}
+else{
+    Console.Write("Tam sayi degildir.");
+}
