8aa701cbbf21015b75282c3882d44cc63e3e6f95
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 14:35:12 +0300

    OperatorsuzUsAlma added

---
+++ OperatorsuzUsAlma/Program.cs
@@ -0,0 +1,13 @@
+﻿int sayi1, sayi2, sonuc=1;
+
+Console.Write("Ilk sayi degerini giriniz: ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ikinci sayi degerini giriniz: ");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=sayi2; i++){
+    sonuc*=sayi1;
+}
+
+Console.Write("{0} ^ {1} = {2}",sayi1, sayi2, sonuc);
+
