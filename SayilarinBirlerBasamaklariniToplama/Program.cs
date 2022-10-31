7bdb97aa413ce5d1f5d863adb7d998799efc2d04
berkaykarakush <berkaykaraks@outlook.com>
2022-10-31 15:35:28 +0300

    SayilarinBirlerBasamaklariniToplama added

---
+++ SayilarinBirlerBasamaklariniToplama/Program.cs
@@ -0,0 +1,12 @@
+﻿int n, sayi, toplam=0, i=0;
+
+Console.Write("Kac adet sayi degeri gireceksiniz: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+while(i<n){
+    Console.Write("Sayi degeri giriniz: ");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    toplam = toplam + sayi % 10;
+    i++;
+}
+Console.Write("\nToplam: {0}",toplam);
