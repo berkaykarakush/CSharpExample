f3f86dec1c85608446cf4b29c73f49a5b30f96e4
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 14:37:47 +0300

    RekursifFaktoriyel added

---
+++ RekursifFaktoriyel/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{   public static int faktoriyel(int k)
+    {
+        if(k<=1){
+            return 1;
+        }
+        else{
+            return k*faktoriyel(k-1);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        int sayi;
+        Console.Write("Pozitif tamsayi degeri giriniz: ");
+        sayi = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\n{0}!={1}",sayi,faktoriyel(sayi));
+    }
+}
