7481bf797685761dcea85ac6f6f038354e805b08
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 10:50:22 +0300

    NSayidanEnBuyukCiftSayiyiBulma added

---
+++ NSayidanEnBuyukCiftSayiyiBulma/Program.cs
@@ -0,0 +1,20 @@
+﻿int n, enb=-1, sayi;
+
+Console.Write("Kac adet sayi gireceksiniz ?: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=n;){
+    Console.Write("Sayi degerini giriniz: ");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    if((sayi%2==0) && (sayi>enb)){
+        enb = sayi;
+    }
+    i++;
+}
+
+if(enb == -1){
+    Console.Write("Cift tamsayi yok.");
+}
+else{
+    Console.Write("\n En buyuk cift tamsayi: {0}",enb);
+}
