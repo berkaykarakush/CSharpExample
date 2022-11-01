475fe77f7d887fe9950fe1b9aef31997dd4f7989
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 12:54:18 +0300

    UcVeBesinKatlari added

---
+++ UcVeBesinKatlari/Program.cs
@@ -0,0 +1,12 @@
+﻿int s;
+
+Console.Write("3i + 5j = S\nS degerini giriniz: ");
+s = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<=s/3; i++){
+    for(int j=0; j<=s/5; j++){
+        if(s == 3*i+5*j){
+            Console.Write("{0},{1}\n",i,j);
+        }
+    }
+}
