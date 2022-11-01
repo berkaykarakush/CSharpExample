0733f2b4dd9c014b9b6ba40e8320830a3234dda4
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 13:21:53 +0300

    BasamakToplamiBasamakSayisinaEsitOlanSayilar added

---
+++ BasamakToplamiBasamakSayisinaEsitOlanSayilar/Program.cs
@@ -0,0 +1,11 @@
+﻿//100-999 arasi
+ 
+ for(int i=1; i<10; i++){
+    for(int j=0; j<10; j++){
+        for(int k=0; k<10; k++){
+            if(i+j+k == 3){
+                Console.WriteLine(100*i+10*j+k);
+            }
+        }
+    }
+ }
