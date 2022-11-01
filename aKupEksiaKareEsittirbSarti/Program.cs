b478820eedc7f2b5955f1aab36b58ed12e4dea36
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 12:37:23 +0300

    aKupEksiaKareEsittirbSarti added

---
+++ aKupEksiaKareEsittirbSarti/Program.cs
@@ -0,0 +1,17 @@
+﻿int b;
+
+Console.Write("a^3 - a^2 = b \nb degerini giriniz: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(b>0 && b<100){
+    for(int a=1; a<100; a++){
+    if(Math.Pow(a,3)-Math.Pow(a,2)==b){
+        Console.Write("a: {0}",a);
+        }
+    }
+}
+else
+{
+    Console.Write("0-100 arasinda oyle bir tamsayi yoktur.");
+}
+
