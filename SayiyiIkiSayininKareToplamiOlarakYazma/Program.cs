bf594ab66f491a54931783c47dd162f1a73e51d7
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 12:45:45 +0300

    SayiyiIkiSayininKareToplamiOlarakYazma added

---
+++ SayiyiIkiSayininKareToplamiOlarakYazma/Program.cs
@@ -0,0 +1,12 @@
+﻿int sayi;
+
+Console.Write("Sayi degerini giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+for(int i=0; i<=sayi; i++){
+    for(int j=0; j<=sayi; j++){
+        if(i*i+j*j == sayi){
+            Console.Write("{0}-{1}\n",i,j);
+        }
+    }
+}
