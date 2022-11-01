9d3e846cc72a19d447e22e1d5ee269ed03d3914c
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 13:16:38 +0300

    KendiHanelerineTamBolunenSayilar added

---
+++ KendiHanelerineTamBolunenSayilar/Program.cs
@@ -0,0 +1,16 @@
+﻿int baslangic, bitis, toplamSayi;
+
+Console.Write("Baslangic degerini giriniz: ");
+baslangic = Convert.ToInt32(Console.ReadLine());
+Console.Write("Bitis degerini giriniz: ");
+bitis = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<baslangic; i++){
+    for(int j=1; j<baslangic; j++){
+        toplamSayi = baslangic*i+j;
+        if((toplamSayi%i==0) && (toplamSayi%j==0)){
+            Console.WriteLine(toplamSayi);
+        }
+    }
+}
+
