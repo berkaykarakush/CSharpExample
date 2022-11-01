d4c704493fff739a893ff12f8d34745c433b8123
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 13:29:31 +0300

    IlkNBasamakToplamiSonNBasamakToplaminaEsitSayilar added

---
+++ IlkNBasamakToplamiSonNBasamakToplaminaEsitSayilar/Program.cs
@@ -0,0 +1,12 @@
+﻿//1000-9999 arasi
+for(int i=1; i<10; i++){
+    for(int j=0; j<10; j++){
+        for(int k=0; k<10; k++){
+            for(int m=0; m<10; m++){
+                if(i+j == k+m){
+                    Console.WriteLine(1000*i+100*j+10*k+m);
+                }
+            }
+        }
+    }
+}
