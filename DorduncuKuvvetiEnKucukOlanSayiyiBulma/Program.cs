69927d28a862c025ced9e0ae8f3e046f1c4e3f92
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 12:19:45 +0300

    DorduncuKuvvetiEnKucukOlanSayiyiBulma added

---
+++ DorduncuKuvvetiEnKucukOlanSayiyiBulma/Program.cs
@@ -0,0 +1,10 @@
+﻿int n;
+double sayi, aranan;
+
+Console.Write("Basamak sayisi: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+sayi = Math.Pow(Math.Pow((Math.Pow(10,n-1)),(0.25)),4);
+aranan = Math.Pow(sayi,(0.25));
+
+Console.Write("a: {0:F2}\ns:{1:F2}",aranan, sayi);
