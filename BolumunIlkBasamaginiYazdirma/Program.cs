6a6b489752cda0ddaee8bfdb1b7f56fb3b255a16
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 10:42:24 +0300

    BolumunIlkBasamaginiYazdirma added

---
+++ BolumunIlkBasamaginiYazdirma/Program.cs
@@ -0,0 +1,14 @@
+﻿int sayi1, sayi2, kalan=0, r=1;
+
+Console.Write("Ilk sayi degerini giriniz: ");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ikinci sayi degerini giriniz: ");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\n0.");
+while(kalan != sayi2){
+    Console.Write((10*r)/sayi1);
+    r = 10*r%sayi1;
+    kalan+=1;
+}
+
