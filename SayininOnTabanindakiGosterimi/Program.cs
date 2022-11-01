a9cf2cbd7ffdab1ea762eadab73f57994c21bd70
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 10:34:10 +0300

    SayininOnTabanindakiGosterimi added

---
+++ SayininOnTabanindakiGosterimi/Program.cs
@@ -0,0 +1,11 @@
+﻿int sayi, kalan, onTabanindakiSayi=0;
+
+Console.Write("Sayi degeri giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+kalan = sayi;
+while(kalan!=0){
+    Console.Write(kalan%10 + ".10^" + onTabanindakiSayi + " + ");
+    kalan = kalan/10;
+    onTabanindakiSayi+=1;
+}
