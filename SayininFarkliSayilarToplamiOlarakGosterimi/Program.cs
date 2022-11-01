21a588b093fbe62ae2a2c47745375e0c9a9a9300
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 13:56:35 +0300

    SayininFarkliSayilarToplamiOlarakGosterimi added

---
+++ SayininFarkliSayilarToplamiOlarakGosterimi/Program.cs
@@ -0,0 +1,20 @@
+﻿int sayi, b, s=0;
+
+Console.Write("Sayi degerini giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+b = sayi - 3;
+
+for(int i=1; i<b; i++){
+    for(int j=i; j<b; j++){
+        for(int k=j; k<b; k++){
+            for(int l=k; l<b; l++){
+                if(sayi == i+j+k+l){
+                    s+=1;
+                    Console.WriteLine("{0},{1},{2},{3}",i,j,k,l);
+                }
+            }
+        }
+    }
+}
+
