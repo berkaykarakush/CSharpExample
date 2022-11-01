207c56a020a395636714ce2cda59da6e4822cc28
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 14:44:16 +0300

    McCarthy91Fonksiyonu added

---
+++ McCarthy91Fonksiyonu/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    public static int McCarthy91(int k){
+        if(k>100){
+            return k-10;
+        }
+        else{
+            return McCarthy91(McCarthy91(k+11));
+        }
+    }
+
+    private static void Main(string[] args){
+        int n;
+        Console.Write("Deger: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nMcCarthy 91 Fonksiyon degeri: {0}",McCarthy91(n));
+    }
+}
