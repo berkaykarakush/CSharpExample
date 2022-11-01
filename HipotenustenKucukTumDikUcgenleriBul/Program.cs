160796e2f279e85c4dc133da90628e2490b253d2
berkaykarakush <berkaykaraks@outlook.com>
2022-11-01 13:03:24 +0300

    HipotenustenKucukTumDikUcgenleriBul added

---
+++ HipotenustenKucukTumDikUcgenleriBul/Program.cs
@@ -0,0 +1,13 @@
+﻿int hipotenus;
+
+Console.Write("Hipotenus: ");
+hipotenus = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=hipotenus; i++){
+    for(int j=i; j<=hipotenus; j++){
+        double c = Math.Sqrt(Math.Pow(i,2)+Math.Pow(j,2));
+        if((c<hipotenus) && (c == (int)c)){
+            Console.Write("a: {0}, b:{1}, c:{2}\n",i,j,c);
+        }
+    }
+}
